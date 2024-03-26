876fb6286c29bad6ba2a7938e9c74e9ef64db532
Blazej_Kujawinski <blakuj@gmail.com>
2024-03-26 16:51:03 +0100

    ageValidation Implementation

--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -24,5 +24,22 @@ namespace LegacyApp
             return true;
             
         }
+
+       
+
+        private bool ageValidation(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
     }
 }
